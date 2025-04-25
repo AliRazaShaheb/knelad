df6eabad6973ca357fa2078ceeb0e85f35a7aec8
Ali Raza Shaheb <arz.shaheb@gmail.com>
2025-04-25 15:37:37 +0530

    latest setup changes changed cms url

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,37 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { GraphQLClient, gql } from 'graphql-request'
+import { GraphQLClient, gql } from "graphql-request";
 const key = process.env.NEXT_PUBLIC_SECRET_KEY;
 
 const graphqlAPI = `https://api-ap-northeast-1.graphcms.com/v2/${key}/master`;
+// const graphqlAPI = process.env.NEXT_PUBLIC_URI;
 
 const token = process.env.NEXT_PUBLIC_TOKEN;
 
 export default async function comments(req, res) {
   // res.status(200).json({ name: 'John Doe' })
 
-
   const gqlClient = new GraphQLClient(graphqlAPI, {
-    headers:{
-      authorization:`Bearer ${token}`
-    }
-  })
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
 
   const query = gql`
-    mutation CreateComment($name:String!, $email:String!, $comment:String!, $slug:String!){
-      createComment(data:{name:$name, email:$email, comment:$comment, post:{ connect:{ slug:$slug } }}){
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
         id
       }
     }
-  `
-    try {
-      const result = await gqlClient.request(query, req.body)
-      res.status(200).send(result)
-    } catch (error) {
-      res.status(500).send(error)
-    }
-
-
-  
-
+  `;
+  try {
+    const result = await gqlClient.request(query, req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
--- services/index.js
+++ services/index.js
@@ -1,8 +1,8 @@
-import { request, gql } from 'graphql-request';
+import { request, gql } from "graphql-request";
 const key = process.env.NEXT_PUBLIC_SECRET_KEY;
 
-
 const graphqlAPI = `https://api-ap-northeast-1.graphcms.com/v2/${key}/master`;
+// const graphqlAPI = process.env.NEXT_PUBLIC_URI;
 
 export const getPosts = async () => {
   const query = gql`
@@ -44,28 +44,28 @@ export const getPosts = async () => {
 export const getCategories = async () => {
   const query = gql`
     query GetCategories {
-        categories {
-          name
-          slug
-        }
+      categories {
+        name
+        slug
+      }
     }
   `;
 
   const result = await request(graphqlAPI, query);
-  
+
   return result.categories;
 };
 
 export const getPostDetails = async (slug) => {
   const query = gql`
-    query GetPostDetails($slug : String!) {
-      post(where: {slug: $slug}) {
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
         title
         excerpt
         featuredImage {
           url
         }
-        author{
+        author {
           name
           bio
           photo {
@@ -94,7 +94,10 @@ export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
-        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
         last: 3
       ) {
         title
@@ -113,11 +116,11 @@ export const getSimilarPosts = async (categories, slug) => {
 
 export const getAdjacentPosts = async (createdAt, slug) => {
   const query = gql`
-    query GetAdjacentPosts($createdAt:DateTime!,$slug:String!) {
-      next:posts(
+    query GetAdjacentPosts($createdAt: DateTime!, $slug: String!) {
+      next: posts(
         first: 1
         orderBy: createdAt_ASC
-        where: {slug_not: $slug, AND: {createdAt_gte: $createdAt}}
+        where: { slug_not: $slug, AND: { createdAt_gte: $createdAt } }
       ) {
         title
         featuredImage {
@@ -126,10 +129,10 @@ export const getAdjacentPosts = async (createdAt, slug) => {
         createdAt
         slug
       }
-      previous:posts(
+      previous: posts(
         first: 1
         orderBy: createdAt_DESC
-        where: {slug_not: $slug, AND: {createdAt_lte: $createdAt}}
+        where: { slug_not: $slug, AND: { createdAt_lte: $createdAt } }
       ) {
         title
         featuredImage {
@@ -149,7 +152,7 @@ export const getAdjacentPosts = async (createdAt, slug) => {
 export const getCategoryPost = async (slug) => {
   const query = gql`
     query GetCategoryPost($slug: String!) {
-      postsConnection(where: {categories_some: {slug: $slug}}) {
+      postsConnection(where: { categories_some: { slug: $slug } }) {
         edges {
           cursor
           node {
@@ -178,7 +181,7 @@ export const getCategoryPost = async (slug) => {
     }
   `;
   const result = await request(graphqlAPI, query, { slug });
-  
+
   return result.postsConnection.edges;
 };
 
@@ -208,10 +211,10 @@ export const getFeaturedPosts = async () => {
 };
 
 export const submitComment = async (obj) => {
-  const result = await fetch('/api/comments', {
-    method: 'POST',
+  const result = await fetch("/api/comments", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(obj),
   });
@@ -219,24 +222,21 @@ export const submitComment = async (obj) => {
   return result.json();
 };
 
-
 export const getComments = async (slug) => {
   const query = gql`
-    query GetComments($slug:String!) {
-        comments(where:{ post:{ slug:$slug } }) {
-          name
-          createdAt
-          comment
-        }
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
     }
   `;
 
-  const result = await request(graphqlAPI, query, {slug});
+  const result = await request(graphqlAPI, query, { slug });
 
   return result.comments;
 };
-
-
 
 // export const getComments = async (slug) => {
 //   const query = gql`
